51786646e0b8a4e6b84c76b89aba0bea824c3662
tadanoluka <simplygod39@gmail.com>
2022-10-24 10:56:00 +0200

    Stage 5/5

--- Main.kt
+++ Main.kt
@@ -2,11 +2,10 @@ package tictactoe
 
 import kotlin.math.abs
 
-enum class TicTacToeSymbols(val possibleSymbols: List<Char>) {
+enum class TicTacToeSymbols(private val possibleSymbols: List<Char>) {
     X_MARK(listOf('X', 'x', '+')),
     O_MARK(listOf('O', 'o', '0')),
-    EMPTY_CELL(listOf(' ', '_', '-')),
-    NULL(listOf());
+    EMPTY_CELL(listOf(' ', '_', '-'));
 
     fun getDefChar() = possibleSymbols[0]
 }
@@ -22,14 +21,13 @@ enum class GameStates(private val description: String, private val debugString: 
     O_WINS("The grid has three O’s in a row (including diagonals).","O wins"),
     IMPOSSIBLE("the grid has three X’s in a row as well as three O’s in a row, " +
             "or there are a lot more X's than O's or vice versa.",
-        "Impossible"),
-    NULL("","");
+        "Impossible");
 
     fun getOutputString() = debugString
 }
 
 class GameGrid(private val grid: MutableList<MutableList<TicTacToeSymbols>>){
-    val currentGameState: GameStates = calculateCurrentGameState()
+    var currentGameState: GameStates = calculateCurrentGameState()
 
     private var winnerIsX = false
     private var winnerIsO = false
@@ -44,7 +42,7 @@ class GameGrid(private val grid: MutableList<MutableList<TicTacToeSymbols>>){
     """.trimIndent())
     }
 
-    private fun calculateCurrentGameState(): GameStates {
+    fun calculateCurrentGameState(): GameStates {
         winnerIsX = isCharIsAWinner(TicTacToeSymbols.X_MARK)
         winnerIsO = isCharIsAWinner(TicTacToeSymbols.O_MARK)
 
@@ -111,15 +109,23 @@ class GameGrid(private val grid: MutableList<MutableList<TicTacToeSymbols>>){
 }
 
 fun main() {
-    val inputString = readln()
-    if (isInputStringValid(inputString)) {
-        val listOfTicTacToeSymbols = parseInputString(inputString)
-        val grid = makeGrid(listOfTicTacToeSymbols)
-        val gameGrid = GameGrid(grid)
-        gameGrid.displayGrid()
-        // println(gameGrid.currentGameState.getOutputString())
-        userMakeMove(gameGrid, TicTacToeSymbols.X_MARK)
-        gameGrid.displayGrid()
+    val grid = makeEmptyGrid()
+    val gameGrid = GameGrid(grid)
+    gameGrid.displayGrid()
+    var turnSide: TicTacToeSymbols = TicTacToeSymbols.X_MARK
+    while (true) {
+        if (gameGrid.calculateCurrentGameState() == GameStates.IS_RUN) {
+            userMakeMove(gameGrid, turnSide)
+            gameGrid.displayGrid()
+            turnSide = if (turnSide == TicTacToeSymbols.X_MARK) {
+                TicTacToeSymbols.O_MARK
+            } else {
+                TicTacToeSymbols.X_MARK
+            }
+        } else {
+            println(gameGrid.calculateCurrentGameState().getOutputString())
+            break
+        }
     }
 
 }
@@ -140,52 +146,12 @@ fun userMakeMove(gameGrid: GameGrid, userSide: TicTacToeSymbols) {
     }
 }
 
-
-fun isInputStringValid(inputString: String): Boolean {
-    if (inputString.count() == 9) {
-        val possibleCharsList = mutableListOf<Char>()
-        for (enum in TicTacToeSymbols.values()) {
-            for (char in enum.possibleSymbols) {
-                possibleCharsList.add(char)
-            }
-        }
-        for (char in inputString) {
-            if (char !in possibleCharsList) {
-                println("The input string contains an unknown character - \'$char\'")
-                val possibleSymbolsStringList = mutableListOf<String>()
-                for (item in possibleCharsList) {
-                    possibleSymbolsStringList.add("\'$item\'")
-                }
-                println("The input string must consist only of the characters: ${possibleSymbolsStringList.joinToString()}")
-                return false
-            }
-        }
-    } else {
-        println("The length of the input string must be equal to 9 characters.")
-        return false
-    }
-    return true
-}
-
-fun parseInputString(inputString: String): MutableList<TicTacToeSymbols> {
-    val mListOfTTTSymbols: MutableList<TicTacToeSymbols> = mutableListOf()
-    for (char in inputString) {
-        when (char) {
-            in TicTacToeSymbols.X_MARK.possibleSymbols -> mListOfTTTSymbols.add(TicTacToeSymbols.X_MARK)
-            in TicTacToeSymbols.O_MARK.possibleSymbols -> mListOfTTTSymbols.add(TicTacToeSymbols.O_MARK)
-            in TicTacToeSymbols.EMPTY_CELL.possibleSymbols -> mListOfTTTSymbols.add(TicTacToeSymbols.EMPTY_CELL)
-            else -> mListOfTTTSymbols.add(TicTacToeSymbols.NULL)
-        }
-    }
-    return mListOfTTTSymbols
-}
-
-fun makeGrid(inputList: MutableList<TicTacToeSymbols>): MutableList<MutableList<TicTacToeSymbols>> {
+fun makeEmptyGrid(): MutableList<MutableList<TicTacToeSymbols>> {
     val grid: MutableList<MutableList<TicTacToeSymbols>> = mutableListOf()
     for (i in 0..2) {
         val row = mutableListOf<TicTacToeSymbols>()
         for (j in 0..2) {
-            row.add(inputList[j + i * 3])
+            row.add(TicTacToeSymbols.EMPTY_CELL)
         }
         grid.add(row)
     }
